1ffe7dbad26f1cf3ee1fa86e2968f5e3a3e3db4e
Roya Delparastaran <roya.delparastaran@tealium.com>
2020-09-09 20:24:52 -0700

    simplify

--- kochava/src/main/java/com/tealium/kochava/KochavaTrackable.kt
+++ kochava/src/main/java/com/tealium/kochava/KochavaTrackable.kt
@@ -9,57 +9,7 @@ interface KochavaTrackable {
 
     fun setSleep(sleep: Boolean)
 
-    fun logEvent(
-        eventName: String,
-        deviceType: String,
-        placement: String,
-        adType: String,
-        adCampaignId: String,
-        adCampaignName: String,
-        adSize: String,
-        adGroupName: String,
-        adGroupId: String,
-        adNetworkName: String,
-        adMediationName: String,
-        checkoutAsGuest: String,
-        contentId: String,
-        contentType: String,
-        currency: String,
-        date: String,
-        description: String,
-        destination: String,
-        duration: Double,
-        source: String,
-        uri: String,
-        completed: Boolean,
-        action: String,
-        background: Boolean,
-        spatialX: Double,
-        spatialY: Double,
-        spatialZ: Double,
-        validated: String,
-        userName: String,
-        userId: String,
-        success: String,
-        startDate: String,
-        endDate: String,
-        searchTerm: String,
-        source1: String,
-        score: String,
-        results: String,
-        registrationMethod: String,
-        referralFrom: String,
-        receiptId: String,
-        ratingValue: Double,
-        quantity: Double,
-        price: Double,
-        origin: String,
-        orderId: String,
-        name: String,
-        maxRatingValue: Double,
-        level: String,
-        itemAddedFrom: String
-    )
+    fun standardEvent(eventName: String, parameters: String)
 
     fun customEvent(eventName: String, parameters: String)
 }
--- kochava/src/main/java/com/tealium/kochava/KochavaTracker.kt
+++ kochava/src/main/java/com/tealium/kochava/KochavaTracker.kt
@@ -1,6 +1,5 @@
 package com.tealium.kochava
 
-import android.annotation.SuppressLint
 import android.app.Application
 import com.kochava.base.Tracker
 import org.json.JSONObject
@@ -23,135 +22,39 @@ class KochavaTracker(application: Application, applicationId: String) : KochavaT
         Tracker.setSleep(sleep)
     }
 
-    @SuppressLint("CheckResult")
-    override fun logEvent(
+    override fun standardEvent(
         eventName: String,
-        deviceType: String,
-        placement: String,
-        adType: String,
-        adCampaignId: String,
-        adCampaignName: String,
-        adSize: String,
-        adGroupName: String,
-        adGroupId: String,
-        adNetworkName: String,
-        adMediationName: String,
-        checkoutAsGuest: String,
-        contentId: String,
-        contentType: String,
-        currency: String,
-        date: String,
-        description: String,
-        destination: String,
-        duration: Double,
-        source: String,
-        uri: String,
-        completed: Boolean,
-        action: String,
-        background: Boolean,
-        spatialX: Double,
-        spatialY: Double,
-        spatialZ: Double,
-        validated: String,
-        userName: String,
-        userId: String,
-        success: String,
-        startDate: String,
-        endDate: String,
-        searchTerm: String,
-        source1: String,
-        score: String,
-        results: String,
-        registrationMethod: String,
-        referralFrom: String,
-        receiptId: String,
-        ratingValue: Double,
-        quantity: Double,
-        price: Double,
-        origin: String,
-        orderId: String,
-        name: String,
-        maxRatingValue: Double,
-        level: String,
-        itemAddedFrom: String
+        parameters: String
     ) {
-        var event: Tracker.Event
         if (eventName.equals(Commands.ACHIEVEMENT)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_ACHIEVEMENT)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_ACHIEVEMENT).toString(), parameters)
         } else if (eventName.equals(Commands.AD_VIEW)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_AD_VIEW)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_AD_VIEW).toString(), parameters)
         } else if (eventName.equals(Commands.ADD_TO_CART)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_ADD_TO_CART)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_ADD_TO_CART).toString(), parameters)
         } else if (eventName.equals(Commands.ADD_TO_WISH_LIST)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_ADD_TO_WISH_LIST)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_ADD_TO_WISH_LIST).toString(), parameters)
         } else if (eventName.equals(Commands.CHECKOUT_START)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_CHECKOUT_START)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_CHECKOUT_START).toString(), parameters)
         } else if (eventName.equals(Commands.DEEP_LINK)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_DEEP_LINK)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_DEEP_LINK).toString(), parameters)
         } else if (eventName.equals(Commands.LEVEL_COMPLETE)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_LEVEL_COMPLETE)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_LEVEL_COMPLETE).toString(), parameters)
         } else if (eventName.equals(Commands.PURCHASE)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_PURCHASE)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_PURCHASE).toString(), parameters)
         } else if (eventName.equals(Commands.RATING)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_RATING)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_RATING).toString(), parameters)
         } else if (eventName.equals(Commands.SEARCH)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_SEARCH)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_SEARCH).toString(), parameters)
         } else if (eventName.equals(Commands.START_TRIAL)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_START_TRIAL)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_START_TRIAL).toString(), parameters)
         } else if (eventName.equals(Commands.SUBSCRIBE)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_SUBSCRIBE)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_SUBSCRIBE).toString(), parameters)
         } else if (eventName.equals(Commands.TUTORIAL_COMPLETE)) {
-            event = Tracker.Event(Tracker.EVENT_TYPE_TUTORIAL_COMPLETE)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_TUTORIAL_COMPLETE).toString(), parameters)
         } else {
-            event = Tracker.Event(Tracker.EVENT_TYPE_VIEW)
+            Tracker.sendEvent(Tracker.Event(Tracker.EVENT_TYPE_VIEW).toString(), parameters)
         }
-
-        event.setAction(action)
-            .setAdCampaignId(adCampaignId)
-            .setAdCampaignName(adCampaignName)
-            .setAdDeviceType(deviceType)
-            .setAdGroupId(adGroupId)
-            .setAdGroupName(adGroupName)
-            .setAdMediationName(adMediationName)
-            .setAdPlacement(placement)
-            .setAdSize(adSize)
-            .setAdType(adType)
-            .setCheckoutAsGuest(checkoutAsGuest)
-            .setCompleted(completed)
-            .setContentId(contentId)
-            .setContentType(contentType)
-            .setCurrency(currency)
-            .setStartDate(startDate)
-            .setDescription(description)
-            .setDestination(destination)
-            .setEndDate(endDate)
-            .setItemAddedFrom(itemAddedFrom)
-            .setLevel(level)
-            .setMaxRatingValue(maxRatingValue)
-            .setName(name)
-            .setOrderId(orderId)
-            .setQuantity(quantity)
-            .setOrigin(origin)
-            .setRatingValue(ratingValue)
-            .setReceiptId(receiptId)
-            .setReferralFrom(referralFrom)
-            .setRegistrationMethod(registrationMethod)
-            .setSearchTerm(searchTerm)
-            .setResults(results)
-            .setScore(score)
-            .setDate(date)
-            .setSuccess(success)
-            .setUserId(userId)
-            .setUri(uri)
-            .setValidated(validated)
-            .setBackground(background)
-
-        if (!price.isNaN()) event.setPrice(price)
-        if (!duration.isNaN()) event.setDuration(duration)
-        if (!spatialX.isNaN()) event.setSpatialX(spatialX)
-        if (!spatialX.isNaN()) event.setSpatialX(spatialX)
-        if (!spatialZ.isNaN()) event.setSpatialZ(spatialZ)
-        Tracker.sendEvent(event)
     }
 
     override fun customEvent(eventName: String, parameters: String) {
